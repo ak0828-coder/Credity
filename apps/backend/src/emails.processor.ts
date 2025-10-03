6a34d327dab80a47d0a43e98f38d400e89191f8f
ak0828-coder <alexander.kofler06@gmail.com>
2025-10-03 13:38:41 +0200

    ci: silence no-unused-vars in processor

--- apps/backend/src/emails.processor.ts
+++ apps/backend/src/emails.processor.ts
@@ -3,14 +3,15 @@ import type { Job } from 'bullmq';
 
 @Processor('emails')
 export class EmailsProcessor extends WorkerHost {
-  // nicht async -> sonst eslint "require-await"
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   process(job: Job) {
+    // (sp�ter echte Arbeit; aktuell nur "Stub")
     return Promise.resolve({ ok: true });
   }
 
   @OnWorkerEvent('completed')
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   onCompleted(job: Job) {
     // optionales Logging
-    // console.log("Job completed", job.id);
   }
 }
